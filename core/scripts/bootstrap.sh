28fb43293096864132c40cbb7c93e66bd9c6fb9a
Davide Paro <dparo@outlook.it>
2022-08-04 10:57:14 +0200

    Modified bootstrap

--- core/scripts/bootstrap.sh
+++ core/scripts/bootstrap.sh
@@ -278,10 +278,12 @@ source ~/.bash_profile
 source ~/.bashrc
 
 # Install kitty terminal to the latest version
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
 ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-sed -i "s|Icon=kitty|Icon=$HOME/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty.desktop
+cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 
 # Lazygit: https://github.com/jesseduffield/lazygit
 # simple terminal UI for git commands
