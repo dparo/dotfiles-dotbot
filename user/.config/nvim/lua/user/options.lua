3da6887b5211875e55e16ca31414c5a069d7d537
Davide Paro <dparo@outlook.it>
2022-11-08 07:22:24 +0100

    Changed  nvim laststatus

--- user/.config/nvim/lua/user/options.lua
+++ user/.config/nvim/lua/user/options.lua
@@ -104,7 +104,7 @@ vim.o.updatetime = 1000
 -- Winbar support nvim version 0.8 and above
 if vim.fn.has "nvim-0.8" == 1 then
     vim.o.winbar = "=%=%m %f"
-    vim.o.laststatus = 1
+    vim.o.laststatus = 2
     vim.o.cmdheight = 1
 
     vim.o.spell = false
